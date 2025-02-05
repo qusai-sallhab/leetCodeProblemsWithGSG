f6263d2ad7b9ea9d9f17897e15e490f07f2f28f5
qusai sallhab <qusaisallhab@gmail.com>
2025-02-05 21:08:40 +0200

    Add project files.

---
+++ Majority Element/MajorityElement/MajorityElement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MajorityElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MajorityElement(new int[]{1,1,2,2,1,3,1,3,3,3,3,3,3}));
+        }
+
+        public static int MajorityElement(int[] nums)
+        {
+            int majorityElement=0;
+            int counter = 0;
+            int temp;
+            bool foundFlag=false;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                counter = 0;
+                temp = nums[i];
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (temp == nums[j]) counter++;
+                    if (counter > nums.Length / 2.0f) { majorityElement = temp; foundFlag = true; break; }
+                }
+
+                if (foundFlag) break;
+            }
+
+
+            return majorityElement;
+
+        }
+    }
+}
---
+++ TwoSum/TwoSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write(string.Join(",", TwoSum(new int[] { 3,2,4}, 6)));
+        }
+
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] resultArray=new int[2];
+            int firstNumIndex=0;
+            int secondNumIndex=0;
+            bool flag = false;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (j == i) continue;
+                    if (nums[i] + nums[j] == target) { firstNumIndex = i;secondNumIndex = j; flag = true;break; }
+                }
+
+                if (flag) break;
+            }
+
+            resultArray[0] = firstNumIndex;
+            resultArray[1] = secondNumIndex;
+            return resultArray;
+        }
+
+
+        
+    }
+}
